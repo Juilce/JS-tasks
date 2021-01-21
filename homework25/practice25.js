d142c0d17bcd88b36343f71f56d9aa6ed93cf698
yulia.kotsur@gmail.com <yulia.kotsur@gmail.com>
2021-01-21 18:19:58 +0200

    not all tasks

---
+++ homework25/practice25.js
@@ -0,0 +1,62 @@
+/*
+//task 1
+let Car = {
+    production: "Honda",
+    model: "Accord",
+    year: "2015",
+    avgSpeed: 50,
+
+    show: function() {
+        console.log (this.production +", " +
+            this.model +", " +
+            this.year +", " +
+            this.avgSpeed);
+    },
+
+    time: function(s) {
+        let t = s/this.avgSpeed;
+        if (t > 4){
+            t += Math.trunc(t/4);
+        }
+        alert(t);
+    },
+}
+Car.show();
+
+let s = +prompt("enter length");
+Car.time(s);
+*/
+
+/*
+//task 2
+function number(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+}
+
+function division(one,two) {
+    let sum;
+    if(one.denominator != two.denominator){
+        sum = ((one.numerator * two.denominator) + (two.numerator * one.denominator)) / (one.denominator * two.denominator);
+} else {
+    sum = (one.numerator + two.numerator) / one.denominator;
+}
+    let subtract;
+    if(one.denominator != two.denominator) {
+        subtract = ((one.numerator * two.denominator) - (two.numerator * one.denominator)) / (one.denominator * two.denominator);
+    } else {
+        subtract = (one.numerator - two.numerator) / one.numerator;
+    }
+    let multiply = (one.numerator * two.numerator) / (one.denominator * two.denominator);
+    let divise = (one.numerator * two.denominator) / (two.numerator * one.denominator);
+    console.log(`Сума - ${sum} \n Різниця - ${subtract} \n Добуток - ${multiply} \n Частка - ${divise}`);
+}
+
+let numerator1 = +prompt("Enter numerator 1");
+let denominator1 = +prompt("Enter denominator 1");
+let numerator2 = +prompt("Enter numerator 2");
+let denominator2 = +prompt("Enter denominator 2");
+let one = new number(numerator1, denominator1);
+let two = new number(numerator2, denominator2);
+
+division(one,two); */
--- practice/practice.js
+++ practice/practice.js
@@ -1,1 +1,90 @@
-let 
+/*let result = 1;
+let i = +prompt("Enter number")
+while(i){
+result *= i;
+i--;
+}
+alert(result);*/
+/*
+let num
+do{
+num = +prompt("Enter result of 2+2*2")
+if(num != 6){
+alert("Try again")
+} else{
+    alert("you win")
+}
+}while(num != 6)*/
+
+/*
+function maxNumber(a,b){
+    if(a>b){
+        return a;
+    } else {
+        return b;
+    }
+}
+
+let result = maxNumber(5,9);
+alert(result); */
+
+/*
+function getPover(a, b){
+    return Math.pow(a, b)
+}
+
+let num = +prompt('Enter number');
+let result = getPover(num, 5);
+alert(result); */
+/*
+function getPover(a, symbol, b){
+    if (symbol == '+'){
+        return a + b;
+    } else if (symbol == '-'){
+        return a - b;
+    } else if (symbol == '*'){
+        return a * b;
+    } else if (symbol == '/'){
+        return a / b;
+    } else {
+        alert("You entered the wrong data")
+    }
+}
+
+let num = +prompt('Enter number');
+let sign = prompt('Enter sign');
+let num2 = +prompt('Enter number');
+let result = getPover(num, sign, num2);
+alert(result); */
+
+/* // task 6 -
+ function range(a, b){
+    let sum = 0;
+    /*let min, max;
+    if (a > b){
+        max = a;
+        min = b;
+    } else {
+        max = b;
+        min = a;
+    }*/
+  /*  for(let i = a; i <= b; i++){
+        let elem = i;
+    for (let j = 1; j < elem; j++) { 
+        if (elem % j == 0) {
+            sum += j;
+        }
+    }
+    if(elem == sum){
+        console.log("Досконале число - " + elem)
+    } 
+    }
+ }
+ let num = +prompt("Enter range of numbers. The first number is");
+ let num2 = +prompt("The second number is");
+ range(num, num2); */
+
+
+
+
+
